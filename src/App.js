6d7f6bd058a36f28625704bcc35b3e0ad9df631f
Bichurina Anastasiia <69593629+bratzzie@users.noreply.github.com>
2021-02-27 21:07:38 +0200

    added templates for components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import AddTransaction from './components/AddTransaction';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import Income from './components/Income'
+import TransactionList from './components/TransactionList'
 
 function App() {
   return (
     <div>
-      Hello World
+     <Header />
+     <div className="container" />
+        <Balance />
+        <Income />
+        <TransactionList />
+        <AddTransaction />
     </div>
   );
 }
