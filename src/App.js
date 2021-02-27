11775d1e31e27b65a994c99a7f0bcd74e08468f1
Bichurina Anastasiia <69593629+bratzzie@users.noreply.github.com>
2021-02-27 22:44:37 +0200

    completed first order functions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
-import AddTransaction from './components/AddTransaction';
-import Balance from './components/Balance';
-import Header from './components/Header';
-import Income from './components/Income'
-import TransactionList from './components/TransactionList'
+import "./App.css";
+
+import AddTransaction from "./components/AddTransaction";
+import Balance from "./components/Balance";
+import Header from "./components/Header";
+import Income from "./components/Income";
+import TransactionList from "./components/TransactionList";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <div>
-     <Header />
-     <div className="container" />
+    <GlobalProvider>
+        <Header />
+      <div className="container" />
         <Balance />
         <Income />
         <TransactionList />
         <AddTransaction />
-    </div>
+    </GlobalProvider>
   );
 }
 
