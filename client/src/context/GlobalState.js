ff76b44a162974cad1593626b08310a2d3098a50
Bichurina Anastasiia <69593629+bratzzie@users.noreply.github.com>
2021-02-28 14:00:00 +0200

    connected front

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,7 +1,14 @@
 import React, { createContext, useReducer } from "react";
+import axios from 'axios'
+
 import AppReducer from "./AppReducer";
+import { config } from "dotenv/types";
+
+
 const initialState = {
   transactions: [],
+  error: null,
+  loading: true
 };
 
 export const GlobalContext = createContext(initialState);
@@ -9,14 +16,56 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  function deleteTransaction(id) {
-    dispatch({
-      type: "DELETE_TRANSACTION",
-      payload: id,
-    });
+  async function getTransactions() {
+    try {
+      const res = await axios.get('/api/v1/transactions')
+
+      dispatch({
+        type: 'GET_TRANSACTIONS',
+        payload: res.data.data
+      })
+    } catch (error) {
+      dispatch({
+        type: 'TRANSACTION_ERROR',
+        payload: error.response.data.error
+      })
+    }
   }
 
-  function addTransaction(transaction) {
+ async function deleteTransaction(id) {
+   try {
+     await axios.delete(`/api/v1/transactions/${id}`)
+
+      dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+             });
+
+   } catch (error) {
+     dispatch({
+       type: 'TRANSACTION_ERROR',
+       payload: error.response.data.error
+     })
+   }
+  }
+
+  async function addTransaction(transaction) {
+
+    try {
+      const res = await axios.post('/api/v1/transactions', transaction, config)  
+      
+      dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: res.data.data
+      })      
+   
+    } catch (error) {
+       dispatch({
+        type: 'TRANSACTION_ERROR',
+        payload: error.response.data.error
+      })
+    }
+
     dispatch({
       type: "ADD_TRANSACTION",
       payload: transaction,
@@ -28,7 +77,10 @@ export const GlobalProvider = ({ children }) => {
       value={{
         transactions: state.transactions,
         deleteTransaction,
-        addTransaction,
+        error: state.error,
+        loading: state.loading,
+        getTransactions,
+        addTransaction
       }}
     >
       {children}
